cdd5bb1f313d6d90fb84f10fe0ff305c1a38321f
Gregor Doroschenko <g.doroschenko@gmail.com>
2018-02-13 23:18:34 +0100

    create empty shape component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ShapeComponent } from './shape/shape.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShapeComponent
   ],
   imports: [
     BrowserModule
